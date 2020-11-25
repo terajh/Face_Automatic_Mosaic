9522771a18f1e2fc1a87719711d0c9d7a65becfa
terajoo <terajoohyun@ajou.ac.kr>
2020-11-25 08:46:55 +0000

    backup

--- index.py
+++ index.py
@@ -6,18 +6,18 @@ from luxand import luxand
 client = luxand("9c5fdf210f8e41a086b92d65508bc884")
 
 
-app = Flask(__name__, static_folder='outputs')
+app = Flask(__name__, static_folder='templates')
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
  
 @app.route('/api', methods = ['GET', 'POST'])
 def upload_file():
     image = request.files['uploadFile']
-    image.save(os.path.join('./server/outputs',image.filename))
-    result = client.detect(photo = os.path.join('./server/outputs',image.filename))
+    image.save(os.path.join('./outputs',image.filename))
+    result = client.detect(photo = os.path.join('./outputs',image.filename))
 
     res = []
     for i in range(len(result)):
@@ -26,4 +26,4 @@ def upload_file():
     return {'len':len(res), 'list':res}
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = 3001)
+    app.run(debug = True, host = '0.0.0.0', port = 3000)
--- test.py
+++
@@ -1,7 +0,0 @@
-from luxand import luxand
-
-client = luxand("9c5fdf210f8e41a086b92d65508bc884")
-
-result = client.detect(photo = "./test1.jpg")
-
-print(result)
